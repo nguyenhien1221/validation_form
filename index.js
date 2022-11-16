40f4cb07af63e53db2a7d36857a6999057019e39
NguyenHien <nguyenhien2942000@gmail.com>
2022-11-16 23:19:21 +0700

    fix disable btn bug, update UI

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const nameErr = document.getElementById("name-err");
 const passErr = document.getElementById("pass-err");
 const repassErr = document.getElementById("repass-err");
 
+let isError = false;
+
 const showNotifi = () => {
     const notifi = document.getElementById("popup");
     notifi.classList.remove("disapear");
     setTimeout(() => {
         notifi.classList.add("disapear");
+        window.location.reload();
     }, 3000);
 };
 
@@ -20,6 +23,7 @@ const activeBtn = () => {
     submitBtn.disabled = false;
 };
 
+// check username
 const validName = () => {
     const regexName = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -29,76 +33,87 @@ const validName = () => {
     
     if (nameValue == "") {
         disableBtn();
-        nameErr.innerText = "This field is required";
+        nameErr.innerText = "*This field is required";
+        isError = true;
         return false;
     }
     if (isName) {
+        if (isError) {
+            disableBtn();
+        } else {
+            activeBtn();
+        }
         nameErr.innerText = "";
-        activeBtn();
         return false;
     } else {
         disableBtn();
-        nameErr.innerText = "Invalid name";
+        nameErr.innerText = "*Invalid username";
+        isError = true;
     }
     return true;
 };
 
+// check password
 const validPassword = () => {
     const regexPass = /^(?=.*?[A-Z])(?=.*?[a-z]).{8,32}$/;
-
     const passValue = document.getElementById("password").value;
 
     if (passValue == "") {
-        passErr.innerText = "This field is required";
+        passErr.innerText = "*This field is required";
+        isError = true;
         disableBtn();
         return false;
     }
     if (regexPass.test(passValue)) {
+        if (isError) {
+            disableBtn();
+        } else {
+            activeBtn();
+        }
         passErr.innerText = "";
-        activeBtn();
         return false;
     } else {
         passErr.innerText =
-            "Password must 8-32 long and at least 1 Uppercase and Lowercase";
+            "*Password must 8-32 long and at least 1 Uppercase and Lowercase";
         disableBtn();
+        isError = true;
     }
-
     return true;
 };
 
+//check repassword
 const validRepassword = () => {
     const rePasswordValue = document.getElementById("repassword").value;
     const passValue = document.getElementById("password").value;
 
     if (rePasswordValue == "") {
-        repassErr.innerText = "This field is required";
+        repassErr.innerText = "*This field is required";
+        isError = true;
         disableBtn();
         return false;
     }
     if (passValue != rePasswordValue) {
-        repassErr.innerText = "Password does not match";
+        repassErr.innerText = "*Password does not match";
         disableBtn();
+        isError = true;
         return false;
     } else {
         repassErr.innerText = "";
+        isError = false;
         activeBtn();
     }
     return true;
 };
 
+// submit form
 const btn = document.getElementById("submit");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (!validPassword()) {
-        console.log("false");
-    }
-    if (!validName()) {
-        console.log("false");
-    }
-    if (!validRepassword()) {
+    if (!validPassword() && !validName() && !validRepassword()) {
         console.log("false");
     } else {
         showNotifi();
+        console.log('success');
     }
 });
